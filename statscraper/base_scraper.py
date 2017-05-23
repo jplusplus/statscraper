c8e09d7d8fae748731b97b5f1f77bdf1062ce539
rotsee <leo_wallentin@hotmail.com>
2017-05-23 14:42:15 +0200

    remove most likely unintentional string comparison with is

--- statscraper/base_scraper.py
+++ statscraper/base_scraper.py
@@ -22,9 +22,10 @@ class BaseScraper():
         url = self._urls[self.selection.label]
         r = requests.get(url.format(**params))
         if 'filetype' in params:
-            if params['filetype'] is 'csv':
-                data = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter=';')
-            if params['filetype'] is 'json':
+            if params['filetype'] == 'csv':
+                data = csv.DictReader(r.iter_lines(decode_unicode=True),
+                                      delimiter=';')
+            if params['filetype'] == 'json':
                 data = r.json()
         data = r.text
         return self.selection.load(data)
