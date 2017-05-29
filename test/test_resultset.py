6b69f4fb0c55acacd0c8d091787c07b5aeafce1f
rotsee <leo_wallentin@hotmail.com>
2017-05-29 22:48:06 +0200

    new test for refactored base scraper

---
+++ test/test_resultset.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from statscraper.base_scraper import ResultSet
+from pandas.api import types as ptypes
+
+
+class TestResultSet(TestCase):
+
+    def test_pandas_export(self):
+        result = ResultSet()
+        result.append({'city': "Voi", 'value': 45483})
+        df = result.pandas
+        self.assertTrue(ptypes.is_numeric_dtype(df.value))
