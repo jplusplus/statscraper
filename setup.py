6ffb32a3d0f98075b243ff073e650ffbabefe638
rotsee <leo_wallentin@hotmail.com>
2017-05-29 22:46:50 +0200

    nosetests

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name="statscraper",
     version="0.0.1",
     description="""\
 A base class for building web scrapers for statistical data.\
 """,
+    long_description=readme(),
     url="https://github.com/jplusplus/statscraper",
     author="Leo Wallentin and Jens Finnäs, J++ Stockholm; Robin Lindeborg",
     author_email='stockholm@jplusplus.org',
@@ -16,5 +23,7 @@ A base class for building web scrapers for statistical data.\
         "hashlib",
         "json",
         "pandas",
-    ]
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
