61f89c6d81dc5e17eff567f00300b769e7e8b200
rotsee <leo_wallentin@hotmail.com>
2017-05-29 15:39:22 +0200

    add setuptools support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="statscraper",
+      version="0.0.1",
+      description="""\
+A base class for building web scrapers for statistical data.\
+""",
+      url="https://github.com/jplusplus/statscraper",
+      author="Leo Wallentin and Jens Finnäs, J++ Stockholm; Robin Lindeborg",
+      author_email='stockholm@jplusplus.org',
+      license='MIT',
+      packages=['statscraper'],
+      zip_safe=False)
