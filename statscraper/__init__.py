cac4af75f8b658ebc6d16bf092443897ea67f6d0
rotsee <leo_wallentin@hotmail.com>
2017-05-30 08:34:16 +0200

    import syntax

--- statscraper/__init__.py
+++ statscraper/__init__.py
@@ -1,7 +1,7 @@
 # Exceptions
-from base_scraper import NoSuchItem, InvalidData
+from .base_scraper import NoSuchItem, InvalidData
 
 # Classes
-from base_scraper import ResultSet, Itemslist, AllowedValue, Dimension
-from base_scraper import Item, Collection, Dataset
-from base_scraper import BaseScraper
+from .base_scraper import ResultSet, Itemslist, AllowedValue, Dimension
+from .base_scraper import Item, Collection, Dataset
+from .base_scraper import BaseScraper
