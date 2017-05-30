4d67aa3da93514c2d93fa4971dc249d734174f9b
rotsee <leo_wallentin@hotmail.com>
2017-05-30 08:33:57 +0200

    add test for PXWeb

---
+++ test/test_pxweb_scraper.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from statscraper.scrapers.PXWebScraper import PXWeb
+
+
+class TestPXWeb(TestCase):
+
+    def test_pxweb_scraper(self):
+        """ Setup an run a PXWeb scraper, make sure we get
+            some collections from a reliabel source.
+        """
+        scraper = PXWeb()
+        scraper.base_url="http://pxnet2.stat.fi/pxweb/api/v1/sv/StatFin/"
+        self.assertTrue(len(scraper.items))
