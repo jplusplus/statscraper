11eeb9806b89a28019b6292ac49d6a9d758852a0
rotsee <leo_wallentin@hotmail.com>
2017-05-30 13:56:27 +0200

    shorter docstring

--- test/test_pxweb_scraper.py
+++ test/test_pxweb_scraper.py
@@ -6,8 +6,6 @@ from statscraper.scrapers.PXWebScraper import PXWeb
 class TestPXWeb(TestCase):
 
     def test_pxweb_scraper(self):
-        """ Setup an run a PXWeb scraper, make sure we get
-            some collections from a reliabel source.
-        """
+        """ Extending the basescraper """
         scraper = PXWeb(base_url="http://pxnet2.stat.fi/pxweb/api/v1/sv/StatFin/")
         self.assertTrue(len(scraper.items))
