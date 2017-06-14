e7cf1cfbe593dac5dbce4c4e1de31a3330f4f540
rotsee <leo_wallentin@hotmail.com>
2017-06-14 15:20:07 +0200

    remove unused code

--- statscraper/scrapers/SMHIScraper.py
+++ statscraper/scrapers/SMHIScraper.py
@@ -2,53 +2,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-from statscraper.base_scraper import (BaseScraper, Collection,
-                                      Dataset, Dimension, InvalidData)
+from statscraper.base_scraper import BaseScraper, Collection, Dataset
 
 VERSION = "1.0"
-LEVELS = ["api","parameter"]
+# LEVELS = ["api", "parameter"]
+
 
 class SMHIScraper(BaseScraper):
     base_url = "http://opendata.smhi.se/apidocs/"
 
-
-    def _fetch_itemslist(self):
+    def _fetch_itemslist(self, current_item):
         """ Get a all available apis
         """
-        if self.current_item.is_root:
+        if current_item.is_root:
             html = requests.get(self.base_url).text
             soup = BeautifulSoup(html, 'html.parser')
             for item_html in soup.select(".row .col-md-6"):
                 try:
                     label = item_html.select_one("h2").text
-                except:
+                except Exception:
                     continue
                 yield API(label, html_blob=item_html)
         else:
-            parameter = self._collection_path[0]
-
-            data = requests.get(parameter.url)
-            for resource in self.current_item.blob["resource"]:
+            # parameter = current_item.parent
+            # data = requests.get(parameter.url)
+            for resource in current_item.blob["resource"]:
                 label = u"{}, {}".format(resource["title"], resource["summary"])
                 yield Dataset(label, blob=resource)
 
-
-
-    def _fetch_dimensions(self):
-        parameter = self.current_item
-        import pdb;pdb.set_trace()
-        self._collection_path[0]
-        import pdb;pdb.set_trace()
+    def _fetch_dimensions(self, dataset):
         self.url = "http://opendata-download-{}.smhi.se/api/version/{}/parameter/{}.json"\
-            .format(parameter.key, VERSION, )
+            .format(dataset.key, VERSION, )
         yield("Timepoint")
+
 
 class API(Collection):
     level = "api"
 
     def __init__(self, _id, html_blob=None):
         self.id = _id
-        self.key = html_blob.select_one("a").get("href").replace("/index.html","")
+        self.key = html_blob.select_one("a").get("href").replace("/index.html", "")
         self.url = "http://opendata-download-{}.smhi.se/api/version/{}.json"\
             .format(self.key, VERSION)
 
@@ -61,7 +54,7 @@ class API(Collection):
         error_msg = "Scraper does not support parsing of '{}' yet.".format(self.id)
         try:
             r = requests.get(self.url)
-        except:
+        except Exception:
             # Catch ie. "opendata-download-grid.smhi.se"
             raise NotImplementedError(error_msg)
         if r.status_code == 404:
@@ -70,8 +63,5 @@ class API(Collection):
         return r.json()
 
 
-
 class Parameter(Collection):
     level = "parameter"
-
-
