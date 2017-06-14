913fa4ede7ae6752e941057e36b12affc6616ad2
rotsee <leo_wallentin@hotmail.com>
2017-06-14 11:00:41 +0200

    recieve arguments

--- test/test_base_scraper.py
+++ test/test_base_scraper.py
@@ -5,14 +5,14 @@ from statscraper import BaseScraper, Dataset, Dimension
 
 class Scraper(BaseScraper):
 
-    def _fetch_itemslist(self):
+    def _fetch_itemslist(self, item):
         yield Dataset("Dataset_1")
 
-    def _fetch_dimensions(self):
+    def _fetch_dimensions(self, dataset):
         yield Dimension(u"date")
         yield Dimension(u"municipality")
 
-    def _fetch_data(self, query=None):
+    def _fetch_data(self, dataset, query=None):
         yield {
             "date": "2017-08-10",
             "municipality": "Robertsfors",
