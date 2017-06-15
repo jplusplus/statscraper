266443955c6ff15e9bd5b5f33e04a2c87788906a
rotsee <leo_wallentin@hotmail.com>
2017-06-15 11:41:53 +0200

    updated method name

--- tests/test_smhi_scraper.py
+++ tests/test_smhi_scraper.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from unittest import TestCase
 
 from statscraper.scrapers.SMHIScraper import SMHIScraper, Collection
@@ -6,13 +7,13 @@ from statscraper.scrapers.SMHIScraper import SMHIScraper, Collection
 class TestSMHI(TestCase):
 
     def test_smhi_scraper(self):
-        """  """
+        """Setting up scraper."""
         scraper = SMHIScraper()
         self.assertTrue(len(scraper.items))
 
     def test_fetch_dataset(self):
-        """  """
+        u"""Moving to an “API”."""
         scraper = SMHIScraper()
-        scraper.select("Radar")
+        scraper.move_to("Radar")
 
         self.assertTrue(isinstance(scraper.current_item, Collection))
