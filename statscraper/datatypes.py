85ba3fec147bd939b7dcef6eaa277d855ddf9bbf
rotsee <leo_wallentin@hotmail.com>
2017-06-16 17:49:11 +0200

    datatype handler

---
+++ statscraper/datatypes.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+""" Contains code for parsing datatypes from the statscraper-datatypes repo
+"""
+
+
+class Datatype(object):
+    """Represent a datatype, initiated by id."""
+
+    pass
