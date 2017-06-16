d51db1083939ccdd0cb35cdc4c6b8d850e335a8e
rotsee <leo_wallentin@hotmail.com>
2017-06-16 17:48:44 +0200

    navigate tree

--- tests/test_pxweb_scraper.py
+++ tests/test_pxweb_scraper.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from unittest import TestCase
 
 from statscraper.scrapers.PXWebScraper import PXWeb
@@ -5,7 +7,17 @@ from statscraper.scrapers.PXWebScraper import PXWeb
 
 class TestPXWeb(TestCase):
 
-    def test_pxweb_scraper(self):
-        """ Extending the PXWebScraper """
+    def test_init_scraper(self):
+        """Extending the PXWebScraper."""
         pxscraper = PXWeb(base_url="http://pxnet2.stat.fi/pxweb/api/v1/sv/StatFin/")
         self.assertTrue(len(pxscraper.items))
+
+    def test_navigating_tree(self):
+        """Navigate the tree.."""
+        scraper = PXWeb(base_url="http://pxnet2.stat.fi/pxweb/api/v1/sv/StatFin/")
+        scraper.move_to("Befolkning")\
+               .move_to(u"Födda")\
+               .move_to(u"Befolkningsförändringar efter område 1980 - 2016")
+        data = scraper.fetch()
+        self.assertTrue(len(data))
+
