764ccdbaab4040054a3057862417ddcd1ed28e3e
rotsee <leo_wallentin@hotmail.com>
2017-06-18 13:57:38 +0200

    remove unused class, expose Result class

--- statscraper/__init__.py
+++ statscraper/__init__.py
@@ -2,7 +2,7 @@
 from .base_scraper import NoSuchItem, InvalidData
 
 # Classes
-from .base_scraper import ResultSet, Itemslist, AllowedValue, Dimension
+from .base_scraper import Result, ResultSet, Itemslist, Dimension
 from .base_scraper import Item, Collection, Dataset
 from .base_scraper import BaseScraper
 
