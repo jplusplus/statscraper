786c691da6f256e251152b8ef9a251607c254b23
rotsee <leo_wallentin@hotmail.com>
2017-06-20 15:42:03 +0200

    make pandas test work

--- tests/test_resultset.py
+++ tests/test_resultset.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
 
-from statscraper import ResultSet
+from statscraper import Result, ResultSet
 from pandas.api import types as ptypes
 
 
 class TestResultSet(TestCase):
 
     def test_pandas_export(self):
+        """Get results as pandas dataframe."""
         result = ResultSet()
-        result.append({'city': "Voi", 'value': 45483})
+        result.append(Result(45483, {'city': "Voi"}))
         df = result.pandas
         self.assertTrue(ptypes.is_numeric_dtype(df.value))
