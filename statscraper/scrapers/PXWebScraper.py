6b01692fcdaf0dc01bb32eefcf883200d2864f60
rotsee <leo_wallentin@hotmail.com>
2017-06-27 14:55:05 +0200

    make paths work independent of cursor

--- statscraper/scrapers/PXWebScraper.py
+++ statscraper/scrapers/PXWebScraper.py
@@ -31,16 +31,15 @@ class PXWeb(BaseScraper):
         if "base_url" in kwargs and kwargs["base_url"]:
             self.base_url = kwargs["base_url"]
 
-    @property
-    def _api_path(self):
+    def _api_path(self, item):
         """Get the API path for the current cursor position."""
         if self.base_url is None:
             raise NotImplementedError("base_url not set")
-        path = "/".join([x.blob["id"] for x in self.current_item.path])
+        path = "/".join([x.blob["id"] for x in item.path])
         return "/".join([self.base_url, path])
 
     def _fetch_itemslist(self, item):
-        data = requests.get(self._api_path).json()
+        data = requests.get(self._api_path(item)).json()
 
         for d in data:
             if d["type"] == "l":
@@ -49,7 +48,7 @@ class PXWeb(BaseScraper):
                 yield Dataset(d["text"], blob=d)
 
     def _fetch_dimensions(self, dataset):
-        data = requests.get(self._api_path).json()
+        data = requests.get(self._api_path(dataset)).json()
         try:
             for d in data["variables"]:
                 yield Dimension(d["code"],
@@ -75,7 +74,7 @@ class PXWeb(BaseScraper):
             }
         }
         try:
-            raw = requests.post(self._api_path, json=body)
+            raw = requests.post(self._api_path(dataset), json=body)
             data = raw.json()
         except JSONDecodeError:
             raise InvalidData("No valid response from PX Web. Check your query for spelling errors.")
