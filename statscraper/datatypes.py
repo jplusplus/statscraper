4fda5e645e699f3f53cd422ed2befaf94fc48a7b
rotsee <leo_wallentin@hotmail.com>
2017-06-28 16:11:47 +0200

    read csvv files

--- statscraper/datatypes.py
+++ statscraper/datatypes.py
@@ -2,6 +2,10 @@
 """ Contains code for parsing datatypes from the statscraper-datatypes repo
 """
 
+from glob import iglob
+from itertools import chain
+import os
+
 
 class Datatype(object):
     """Represent a datatype, initiated by id."""
@@ -9,7 +13,18 @@ class Datatype(object):
     allowed_values = []
 
     def __init__(self, id):
+        """Id is a datatype from datatypes.csv."""
         self.id = id
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        value_path = os.path.join(dir_path, "datatypes", "values")
+        files = chain.from_iterable(iglob(os.path.join(root, '*.csv'))
+                                    for root, dirs, files in os.walk(value_path))
+
+        for f in files:
+            print("FILE", f)
+        for root, dirs, files in os.walk("/home/leo/statscraper/statscraper/datatypes/values"):
+            print root, dirs, files
 
     def __str__(self):
         return str(self.id)
