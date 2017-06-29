941ea04ee617646d8f997f6fbf2de84247bda2be
rotsee <leo_wallentin@hotmail.com>
2017-06-29 13:36:04 +0200

    expose exception

--- statscraper/__init__.py
+++ statscraper/__init__.py
@@ -5,7 +5,7 @@ from .base_scraper import NoSuchItem, InvalidData
 from .base_scraper import Result, ResultSet, Itemslist, Dimension, DimensionValue
 from .base_scraper import Item, Collection, Dataset
 from .base_scraper import BaseScraper
-from .datatypes import Datatype
+from .datatypes import Datatype, NoSuchDatatype
 
 # Contants
 from .base_scraper import ROOT, TYPE_DATASET, TYPE_COLLECTION
