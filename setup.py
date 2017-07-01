18cd5c2891e5818cf4334fb4c128f8160eb28ddc
rotsee <leo_wallentin@hotmail.com>
2017-07-01 16:19:50 +0200

    add deps

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 
-version = "1.0.0.dev1"
+version = "1.0.0.dev2"
 
 
 def readme():
     """Import README for use as long_description."""
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
 
@@ -20,15 +20,18 @@ A base class for building web scrapers for statistical data.\
     long_description=readme(),
     url="https://github.com/jplusplus/statscraper",
     author="Leo Wallentin and Jens Finnäs, J++ Stockholm; Robin Lindeborg",
-    author_email='stockholm@jplusplus.org',
-    license='MIT',
-    packages=['statscraper'],
+    author_email="stockholm@jplusplus.org",
+    license="MIT",
+    packages=["statscraper"],
     zip_safe=False,
     install_requires=[
         "pandas",
+        "six",
+        "csvkit",
+        "requests",
     ],
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    test_suite="nose.collector",
+    tests_require=["nose"],
     include_package_data=True,
     download_url="https://github.com/jplusplus/skrejperpark/archive/%s.tar.gz"
                  % version,
