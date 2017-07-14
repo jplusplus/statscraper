28b59a58d79760806daf5706d941af905550fbee
Robin Linderborg <robin.linderborg@gmail.com>
2017-07-14 20:05:01 +0200

    getter setters

--- statscraper/DimensionValue.py
+++ statscraper/DimensionValue.py
@@ -7,10 +7,33 @@ class DimensionValue(BaseScraperObject):
     def __init__(self, value, dimension, label=None):
         """Value can be any type. dimension is a Dimension() object."""
         self.value = value
-        # FIXME make these getter methods
-        self.dimension = dimension
-        self.label = label
-        self.id = dimension.id
+        self._dimension = dimension
+        self._label = label
+        self._id = dimension.id
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        self._label = value
+
+    @property
+    def dimension(self):
+        return self._dimension
+
+    @dimension.setter
+    def dimension(self, value):
+        self._dimension = value
 
     def translate(self, dialect):
         translation = self.value
