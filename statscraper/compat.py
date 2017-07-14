71c2ddda2887e60ba299f450ee3a20bafab8280d
Robin Linderborg <robin.linderborg@gmail.com>
2017-07-14 18:24:12 +0200

    compat StringIO

--- statscraper/compat.py
+++ statscraper/compat.py
@@ -1,8 +1,10 @@
 import six
 
 if six.PY3:
+	from io import StringIO
 	from json import JSONDecodeError
 	unicode = str
 elif six.PY2:
+	from StringIO import StringIO
 	unicode = unicode
 	JSONDecodeError = ValueError
