ce2ff461d93299eccaaf0b417422c912e4540070
rotsee <leo_wallentin@hotmail.com>
2017-07-14 23:45:40 +0200

    use spaces, not tabs

--- statscraper/compat.py
+++ statscraper/compat.py
@@ -1,10 +1,10 @@
 import six
 
 if six.PY3:
-	from io import StringIO
-	from json import JSONDecodeError
-	unicode = str
+    from io import StringIO
+    from json import JSONDecodeError
+    unicode = str
 elif six.PY2:
-	from StringIO import StringIO
-	unicode = unicode
-	JSONDecodeError = ValueError
+    from StringIO import StringIO
+    unicode = unicode
+    JSONDecodeError = ValueError
