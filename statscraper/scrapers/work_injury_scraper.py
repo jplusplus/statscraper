44d5ca532000fb74d5f1837f73cde44ac98cb92d
rotsee <leo_wallentin@hotmail.com>
2017-07-15 14:33:09 +0200

    comments

--- statscraper/scrapers/work_injury_scraper.py
+++ statscraper/scrapers/work_injury_scraper.py
@@ -14,25 +14,22 @@ class WorkInjuries(BaseScraper):
     def initiate_browser(self):
         self.browser = webdriver.Firefox()
         self.browser.get('http://webbstat.av.se')
-        # TODO: Should probably wait for a specific
-        # element to appear on the site
+        # Selenium has trouble understanding when this page is loaded,
+        # so wait for 5 extra seconds, just in case
         self.browser.implicitly_wait(5)
 
     @BaseScraper.on("select")
     def switch_dataset(self, id_):
-
-        # `id_` is equal to `self.current_item.id`
-        # `id_` can be either "Arbetsolycka" or "Arbetssjukdom"
         xpath = "//div[@title='%s']" % id_
+        # `id_` can be either "Arbetsolycka" or "Arbetssjukdom"
         button = self.browser.find_element_by_xpath(xpath)
         button.click()
 
     def _fetch_itemslist(self, item):
-        """ This data can be parsed in different ways,
-        but we chose to define two datasets: Number of work injuries,
-        and number of workrelated diseases.
+        """ We define two datasets:
+        - Number of work injuries ("Arbetsolycka")
+        - Number of workrelated diseases ("Arbetssjukdom")
         """
-        assert item.is_root
         yield Dataset("Arbetsolycka")
         yield Dataset("Arbetssjukdom")
 
