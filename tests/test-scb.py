b2e4f6b3cea49f02d5de12671693888d9d2829c5
rotsee <mejl@leowallentin.se>
2020-03-03 14:19:03 +0100

    test returned value type

--- tests/test-scb.py
+++ tests/test-scb.py
@@ -22,6 +22,17 @@ def test_get_data():
     assert "InkomstTyp" in df.columns
 
 
+def test_values():
+    """Make sure values are numerical."""
+    scraper = SCB()
+    scraper.move_to("HE").move_to("HE0110").move_to("HE0110F").move_to("Tab1DispInkN")
+    data = scraper.fetch({
+      "ContentsCode": ("item", "000002VY"),
+      "InkomstTyp": ("item", "FastInkIn"),
+    }, by="municipality")
+    float(data[0].value.isnumeric())
+
+
 def test_invalid_query():
     """We should raise an error on invalid queries."""
     scraper = SCB()
